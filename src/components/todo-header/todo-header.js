9b153827885e7fc85396abcf873f0653d03f77cd
kardesrecep <recepkardes89@gmail.com>
2023-01-25 00:28:29 +0300

    v1

---
+++ src/components/todo-header/todo-header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./todo-header.css";
+const TodoHeader = ({ title }) => {
+  const newTitle = title || "Todo App"
+  return (
+    <div className="header">
+      <h1>{newTitle}</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/comments">Comments</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default React.memo(TodoHeader);
+
